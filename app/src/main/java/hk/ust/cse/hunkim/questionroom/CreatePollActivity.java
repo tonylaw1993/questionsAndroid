ad4e82093cc85a1667732ac3b5611e9fc184dab9
hychanbb <hoyuichan@gmail.com>
2015-11-22 18:32:53 +0800

    create poll DONE

--- app/src/main/java/hk/ust/cse/hunkim/questionroom/CreatePollActivity.java
+++ app/src/main/java/hk/ust/cse/hunkim/questionroom/CreatePollActivity.java
@@ -4,12 +4,18 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.firebase.client.Firebase;
 import com.firebase.client.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 import hk.ust.cse.hunkim.questionroom.db.DBHelper;
 import hk.ust.cse.hunkim.questionroom.db.DBUtil;
+import hk.ust.cse.hunkim.questionroom.question.Poll;
 
 
 public class CreatePollActivity extends Activity {
@@ -34,18 +40,13 @@ public class CreatePollActivity extends Activity {
 
         //initialized once with an Android context.
        Firebase.setAndroidContext(this);
-
         setContentView(R.layout.activity_createpoll);
-
         Intent intent = getIntent();
         assert (intent != null);
-
         QroomName = intent.getStringExtra(MainActivity.ROOM_NAME);
-
         if (QroomName == null || QroomName.length() == 0) {
             QroomName = "all";
         }
-
         setTitle("Room name: " + QroomName);
 
         //Setup our Firebase mFirebaseRef
@@ -62,6 +63,50 @@ public class CreatePollActivity extends Activity {
         DBHelper mDbHelper = new DBHelper(this);
         dbutil = new DBUtil(mDbHelper);
     }
+
+
+     private boolean isNullString(String s){
+         if (s.length()==0){
+             Toast.makeText(CreatePollActivity.this, "Content is Null ", Toast.LENGTH_SHORT).show();
+             Toast.makeText(CreatePollActivity.this, "Please input again ", Toast.LENGTH_LONG).show();
+             return true;
+         }
+         return false;
+     }
+
+    public void sendpoll(View view) {
+
+        EditText polltitle = (EditText) findViewById(R.id.titleInput);
+        String titleText = polltitle.getText().toString();
+        if (isNullString(titleText)) {
+            return;
+        }
+
+        ArrayList<String> content = new ArrayList<>();
+        int[] ids = new int[]{R.id.polloption1, R.id.polloption2, R.id.polloption3, R.id.polloption4, R.id.polloption5};
+
+        for (int id : ids) {
+            EditText t = (EditText) findViewById(id);
+            String Text = t.getText().toString();
+            if (!(Text.length() == 0)) {
+                content.add(Text);
+            }
+            t.setText("");
+        }
+        String[] options = new String[content.size()];
+        options = content.toArray(options);
+
+
+
+
+
+        Poll poll = new Poll(titleText, options);
+//        for(int x =0; x < poll.getItems().size(); x++)
+//        {System.out.println (poll.getItems().toString());}
+        mFirebaseRef.push().setValue(poll);
+        polltitle.setText("");
+    }
+
 
     //@Override
    // public void onStart() {
